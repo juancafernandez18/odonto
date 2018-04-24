7a5f5c1a2e496f1add81fbd6d330822be00eb330
juancafernandez18 <38479730+juancafernandez18@users.noreply.github.com>
2018-04-24 10:49:20 -0300

    cargo el program

---
+++ odontologia/odontologia/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace odontologia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hola mundo odonto");
+            Console.ReadKey();
+        }
+    }
+}
